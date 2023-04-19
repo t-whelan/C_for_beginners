84207e133f6c610889accbdd4a75e57dee0a57ed
t-whelan <whelan.tionne@gmail.com>
2023-04-19 20:32:41 +0200

    variables.c

---
+++ variables.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+//Create a program to print the variable.
+//Create two variables number1 with value 34 and number2 with value 78.32.
+//Print number1 and number2 with a space in between
+
+int main() {
+  // create integer variable
+int number1 = 34;
+// create double variable
+double number2  = 78.32;
+
+// print variables
+printf("%d",  number1);
+printf (" %lf",number2);
+
+return 0;
+}
